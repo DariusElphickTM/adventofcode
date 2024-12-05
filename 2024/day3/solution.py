4165fbdcbb8b31e4d3edfcd386b2cf6902088174
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-05 15:47:59 +0000

    Part 1. That's more like it Dar

--- 2024/day3/solution.py
+++ 2024/day3/solution.py
@@ -1,5 +1,23 @@
+import re
+import functools
+
 def main():
-    print("Hello world")
+    input_text = read_file("input.txt")
+    valid_commands = re.findall(r'mul\(\d+\,\d+\)', input_text)
+    calculations = list(map(execute_command, valid_commands))
+    total = functools.reduce(lambda a, b: a + b, calculations)
+    print(total)
+
+def execute_command(command):
+    tuples = list(map(int, re.findall(r'\d+', command)))
+    return functools.reduce(lambda a, b: a * b, tuples)
+
+def read_file(file_name):
+    """Reads a text file and returns all of it's contents."""
+    with open(file_name, encoding="utf-8") as file:
+        file_contents = file.read()
+        file.close()
+    return file_contents
 
 if __name__ == "__main__":
     main()
