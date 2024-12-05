575c34177443313e46cdbdc314c62f2b3639666a
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-05 15:29:26 +0000

    Day 3 boilerplate

---
+++ 2024/day3/test.py
@@ -0,0 +1,9 @@
+import unittest
+import solution
+
+class TestSolution(unittest.TestCase):
+    def test_case(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
