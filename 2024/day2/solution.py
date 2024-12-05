f686ce47b19c9f9a8a3ea3a83b4388c5d3ef1bb4
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-05 14:42:47 +0000

    Linting

--- 2024/day2/solution.py
+++ 2024/day2/solution.py
@@ -1,41 +1,45 @@
-import sys
+"""Module providing functions for processing safety reports."""
+
 import re
 
-def main():
-    reports = readFile("1.txt")
-    safeReports = process_reports(reports)
-    print("Safe reports:", safeReports.count(True))
-    
+def main() -> None:
+    """Main Function."""
+    reports = read_file("1.txt")
+    safe_reports = process_reports(reports)
+    print("Safe reports:", safe_reports.count(True))
+
 def process_reports(reports):
-    return list(map(checkSafety, reports))
+    """Processes a list of reports and returns whether they're safe."""
+    return list(map(check_safety, reports))
 
-def checkSafety(report, useDampner = False):
-    reportValues = list(map(int, re.findall(r'\d+', report)))
-    isSafe = True
-    
-    isReportAscending = True
-    for i in range(len(reportValues) - 1):
-        currentDiff = reportValues[i + 1] - reportValues[i]
-        
-        if(abs(currentDiff) < 1 or abs(currentDiff) > 3):
-            isSafe = False
+def check_safety(report, use_dampner = False):
+    """Processes a single report and returns whether it's safe."""
+    report_values = list(map(int, re.findall(r'\d+', report)))
+    is_safe = True
+    is_report_ascending = True
+    for i in range(len(report_values) - 1):
+        current_diff = report_values[i + 1] - report_values[i]
+        if(abs(current_diff) < 1 or abs(current_diff) > 3):
+            is_safe = False
             break
-        
-        isReportCurrentlyAscending = currentDiff > 0
-        
+        is_report_currently_ascending = current_diff > 0
         if i == 0:
-            isReportAscending = isReportCurrentlyAscending
+            is_report_ascending = is_report_currently_ascending
         else:
-            isSafe = isReportAscending == isReportCurrentlyAscending
-            if not isSafe:
+            is_safe = is_report_ascending == is_report_currently_ascending
+            if not is_safe:
                 break
-    return isSafe
+    if not is_safe and use_dampner:
+        return is_safe
+    else:
+        return is_safe
 
-def readFile(filename):
-    file = open(filename, 'r')
-    fileContents = file.readlines()
-    file.close()
-    return fileContents
+def read_file(file_name):
+    """Reads a text file and returns all of it's contents."""
+    with open(file_name, encoding="utf-8") as file:
+        file_contents = file.readlines()
+        file.close()
+    return file_contents
 
 if __name__ == "__main__":
     main()
