57f35c16c182a853c86989c4b11e9cabdb16fd06
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-07 09:41:01 +0000

    Setup

---
+++ 2024/day5/test.py
@@ -0,0 +1,10 @@
+import unittest
+import solution
+
+class TestSolution(unittest.TestCase):
+    def test_parse_input_correctly(self):
+        print("Hello world")
+        self.assertEqual(True, True)
+
+if __name__ == "__main__":
+    unittest.main()
