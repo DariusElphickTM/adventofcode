feb8bcb1932dcc4226a78540b4285dbb4f9e8dd5
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-07 10:38:27 +0000

    String parsing done

--- 2024/day5/solution.py
+++ 2024/day5/solution.py
@@ -1,5 +1,25 @@
+import re
+
 def main():
     print("Hello World")
+    
+def parse_rules(input):
+    parsed_rules = {}
+    rule_list = list(map(lambda rule: list(map(int, rule.split('|'))), re.findall(r'\d+\|\d+', input)))
+    for rule in rule_list:
+        preceding_page = rule[0]
+        succeeding_page = rule[1]
+        
+        if not preceding_page in parsed_rules:
+            parsed_rules[preceding_page] = []
+            
+        if not succeeding_page in parsed_rules[preceding_page]:
+            parsed_rules[preceding_page].append(succeeding_page)
+    return parsed_rules
+
+def parse_updates(input):
+    parsed_updates = list(map(lambda line: list(map(int, re.findall(r'\d+', line))), input.split('\n')))
+    return parsed_updates
 
 if __name__ == "__main__":
     main()
--- 2024/day5/test.py
+++ 2024/day5/test.py
@@ -2,9 +2,54 @@ import unittest
 import solution
 
 class TestSolution(unittest.TestCase):
-    def test_parse_input_correctly(self):
-        print("Hello world")
-        self.assertEqual(True, True)
+    
+    def test_parse_ordering_rules_correctly(self):
+        sample = """47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13"""
+        result = solution.parse_rules(sample)
+        self.assertDictEqual({
+            47: [53, 13, 61, 29],
+            97: [13, 61, 47, 29, 53, 75],
+            75: [29, 53, 47, 61, 13],
+            61: [13, 53, 29],
+            29: [13],
+            53: [29, 13],
+        }, result)
+    
+    def test_parse_updates_correctly(self):
+        sample = """75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"""
+        result = solution.parse_updates(sample)
+        self.assertEqual(len(result), 6)
+        self.assertEqual(len(result[0]), 5)
+        self.assertEqual(result[0][0], 75)
+        self.assertEqual(result[0][2], 61)
+        self.assertEqual(result[0][4], 29)
+        self.assertEqual(len(result[2]), 3)
+        
 
 if __name__ == "__main__":
     unittest.main()
