fd3a637a2fd25015dc39361e3db72a49d565bbaa
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-12 22:16:47 +0000

    Solution is working for the test data

--- 2024/day6/guard_gallivant.py
+++ 2024/day6/guard_gallivant.py
@@ -1,6 +1,9 @@
+import copy
+
 class GuardGallivant():
     def __init__(self, map_string):
         self.parse_map(map_string)
+        self.step_count = 0
     
     def parse_map(self, map_string):
         rows = map_string.split('\n')
@@ -13,9 +16,69 @@ class GuardGallivant():
                     self.room_map[i][j] = '.'
                     break
     
+    def play(self):
+        print("Initial state")
+        self.print_state()
+        self.step_count = 0
+        while True:
+            self.step_count += self.take_step()
+            if self.guard_out_of_bounds(self.guard):
+                print("Finished")
+                self.print_state()
+                break
+            self.print_state()
+    
+    def guard_out_of_bounds(self, guard):
+        return guard['y'] < 0 or guard['y'] >= len(self.room_map) or guard['x'] < 0 or guard['x'] >= len(self.room_map[guard['y']])
+
+    def take_step(self):
+        next_guard_position = self.get_guards_next_position(self.guard)
+        step_count = 0
+        if not self.guard_out_of_bounds(next_guard_position):
+            if self.room_map[next_guard_position['y']][next_guard_position['x']] == '#':
+                self.guard = self.turn_guard(self.guard)
+            else:
+                self.room_map[self.guard['y']][self.guard['x']] = 'X'
+                if not self.room_map[next_guard_position['y']][next_guard_position['x']] == 'X':
+                    step_count = 1
+                self.guard = next_guard_position
+        else:
+            self.guard = next_guard_position
+            step_count = 1
+        return step_count
+    
+    def get_guards_next_position(self, current_guard_position):
+        next_guard_position = current_guard_position.copy()
+        if current_guard_position['facing'] == '^':
+            next_guard_position['y'] -= 1
+        elif current_guard_position['facing'] == '<':
+            next_guard_position['x'] -= 1
+        elif current_guard_position['facing'] == '>':
+            next_guard_position['x'] += 1
+        else:
+            next_guard_position['y'] += 1
+        return next_guard_position
+    
+    def turn_guard(self, guard):
+        next_position = {
+            '^': '>',
+            '>': 'v',
+            'v': '<',
+            '<': '^'
+        }
+        new_guard = guard.copy()
+        new_guard['facing'] = next_position[guard['facing']]
+        return new_guard
+        
+    
+    def print_state(self):
+        print("Step count", self.step_count)
+        print(self)
+    
     def __str__(self):
-        string_representation = self.room_map.copy()
+        string_representation = copy.deepcopy(self.room_map)
         guard = self.guard
-        string_representation[guard['y']][guard['x']] = guard['facing']
+        if not self.guard_out_of_bounds(guard):
+            string_representation[guard['y']][guard['x']] = guard['facing']
         string_representation = list(map("".join, string_representation))
         return "\n".join(string_representation)
--- 2024/day6/test.py
+++ 2024/day6/test.py
@@ -48,6 +48,39 @@ class TestGuardGallivant(unittest.TestCase):
 ........#.
 #.........
 ......#...""", str(self.default_test_gallivanter))
+    
+    def test_get_guards_next_position(self):
+        self.assertDictEqual(
+            {'x': 4, 'y': 5, 'facing': '^'}, self.default_test_gallivanter.get_guards_next_position({'x': 4, 'y': 6, 'facing': '^'})
+        )
+        self.assertDictEqual(
+            {'x': 4, 'y': 7, 'facing': 'v'}, self.default_test_gallivanter.get_guards_next_position({'x': 4, 'y': 6, 'facing': 'v'})
+        )
+        self.assertDictEqual(
+            {'x': 3, 'y': 6, 'facing': '<'}, self.default_test_gallivanter.get_guards_next_position({'x': 4, 'y': 6, 'facing': '<'})
+        )
+        self.assertDictEqual(
+            {'x': 5, 'y': 6, 'facing': '>'}, self.default_test_gallivanter.get_guards_next_position({'x': 4, 'y': 6, 'facing': '>'})
+        )
+    
+    def test_should_play_the_game(self):
+        self.default_test_gallivanter.play()
+        self.assertEqual(41, self.default_test_gallivanter.step_count)
+    
+    def test_should_turn_guard(self):
+        self.assertEqual({'x': 4, 'y': 6, 'facing': '>'}, self.default_test_gallivanter.turn_guard({'x': 4, 'y': 6, 'facing': '^'}))
+        self.assertEqual({'x': 4, 'y': 6, 'facing': 'v'}, self.default_test_gallivanter.turn_guard({'x': 4, 'y': 6, 'facing': '>'}))
+        self.assertEqual({'x': 4, 'y': 6, 'facing': '<'}, self.default_test_gallivanter.turn_guard({'x': 4, 'y': 6, 'facing': 'v'}))
+        self.assertEqual({'x': 4, 'y': 6, 'facing': '^'}, self.default_test_gallivanter.turn_guard({'x': 4, 'y': 6, 'facing': '<'}))
+    
+    def test_should_detect_when_guard_out_of_bounds(self):
+        self.assertTrue(self.default_test_gallivanter.guard_out_of_bounds({'x': 4, 'y': -1, 'facing': '^'}))
+        self.assertTrue(self.default_test_gallivanter.guard_out_of_bounds({'x': 4, 'y': 10, 'facing': 'v'}))
+        self.assertTrue(self.default_test_gallivanter.guard_out_of_bounds({'x': -1, 'y': 6, 'facing': '<'}))
+        self.assertTrue(self.default_test_gallivanter.guard_out_of_bounds({'x': 10, 'y': 6, 'facing': '>'}))
+        self.assertFalse(self.default_test_gallivanter.guard_out_of_bounds({'x': 4, 'y': 6, 'facing': '^'}))
+        self.assertFalse(self.default_test_gallivanter.guard_out_of_bounds({'x': 0, 'y': 9, 'facing': 'v'}))
+        self.assertFalse(self.default_test_gallivanter.guard_out_of_bounds({'x': 9, 'y': 0, 'facing': '<'}))
 
 if __name__ == "__main__":
     unittest.main()
