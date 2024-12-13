47e5fcde69898d442e087590d8f5af9193718292
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-13 14:33:20 +0000

    Removing more side effects

--- 2024/day6/guard_gallivant.py
+++ 2024/day6/guard_gallivant.py
@@ -23,9 +23,11 @@ class GuardGallivant():
         self.step_count = 0
         self.loop_opportunties = 0
         while True:
-            next_step = self.take_step(self.guard, self.room_map)
+            next_step = self.take_step(self.guard)
+            if next_step['step_count'] > 0:
+                self.room_map[self.guard['y']][self.guard['x']] = 'X'
+                self.step_count += next_step['step_count']
             self.guard = next_step['next_guard_state']
-            self.step_count += next_step['step_count']
             if self.guard_out_of_bounds(self.guard):
                 print("Finished")
                 self.print_state()
@@ -34,20 +36,30 @@ class GuardGallivant():
                 self.loop_opportunties += self.seek_loop_opportunity()
     
     def seek_loop_opportunity(self):
-        return 1
+        seeker_guard = self.turn_guard(self.guard)
+        turn_count = 0
+        loop_opportunity = 0
+        while turn_count < 4:
+            next_step = self.take_step(seeker_guard)
+            self.guard = next_step['next_guard_state']
+            self.step_count += next_step['step_count']
+            if self.guard_out_of_bounds(self.guard):
+                print("No loop found before going out of bounds")
+                self.print_state()
+                break
+        return loop_opportunity
     
     def guard_out_of_bounds(self, guard):
         return guard['y'] < 0 or guard['y'] >= len(self.room_map) or guard['x'] < 0 or guard['x'] >= len(self.room_map[guard['y']])
 
-    def take_step(self, current_guard_position, map):
+    def take_step(self, current_guard_position):
         next_guard_position = self.get_guards_next_position(current_guard_position)
         step_count = 0
         if not self.guard_out_of_bounds(next_guard_position):
-            if map[next_guard_position['y']][next_guard_position['x']] == '#':
+            if self.room_map[next_guard_position['y']][next_guard_position['x']] == '#':
                 next_guard_position = self.turn_guard(current_guard_position)
             else:
-                map[current_guard_position['y']][current_guard_position['x']] = 'X'
-                if not map[next_guard_position['y']][next_guard_position['x']] == 'X':
+                if not self.room_map[next_guard_position['y']][next_guard_position['x']] == 'X':
                     step_count = 1
         else:
             step_count = 1
