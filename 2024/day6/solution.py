fed13fcdea59249c36ee21b4273f2e7269cf5aaf
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-13 16:25:01 +0000

    Running part 2. Fail :(

--- 2024/day6/solution.py
+++ 2024/day6/solution.py
@@ -4,8 +4,9 @@ def main():
     print("Here we go!")
     input_string = read_file("input.txt")
     gallivanter = guard_gallivant.GuardGallivant(input_string)
-    gallivanter.play()
+    gallivanter.play(True)
     print("Result", gallivanter.step_count)
+    print("Loop opportunity count", len(gallivanter.loop_opportunties))
 
 def read_file(file_name):
     """Reads a text file and returns all of it's contents."""
