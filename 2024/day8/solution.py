fd8ee196854a7ec409e505aaa5a1a4452a56e5e1
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-14 20:35:13 +0000

    Part 1 done :)

---
+++ 2024/day8/solution.py
@@ -0,0 +1,17 @@
+import huge_antenna
+
+def main():
+    print("Here we go!")
+    input_string = read_file("input.txt")
+    my_huge_antenna = huge_antenna.HugeAntenna(input_string)
+    print("Result", my_huge_antenna.get_antinode_count())
+
+def read_file(file_name):
+    """Reads a text file and returns all of it's contents."""
+    with open(file_name, encoding="utf-8") as file:
+        file_contents = file.read()
+        file.close()
+    return file_contents
+
+if __name__ == "__main__":
+    main()
