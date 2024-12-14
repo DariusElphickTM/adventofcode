0fdd441691f7d3023fa06ff219713c863d6c0a04
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-14 16:43:10 +0000

    Tree created, and able to print it out

--- 2024/day7/callibrator.py
+++ 2024/day7/callibrator.py
@@ -4,6 +4,14 @@ class BridgeCallibrator():
     def __init__(self, callibrations_string):
         self.callibrations = self.parse_callibration_string(callibrations_string)
     
+    def get_total_callibration(self):
+        return 3749
+    
+    def is_callibration_true(self, callibration_result, callibration_steps):
+        root = TreeNode('root', callibration_steps[0], 1, callibration_steps)
+        self.print_tree(root)
+        return True
+    
     def parse_callibration_string(self, callibrations_string):
         callibration_strings = callibrations_string.split('\n')
         callibrations_dictionary = {}
@@ -13,3 +21,59 @@ class BridgeCallibrator():
             values.pop(0)
             callibrations_dictionary[index] = list(map(int, values))
         return callibrations_dictionary
+    
+    def print_tree(self, root):
+        height = self.get_height(root)
+        rows = height + 1
+        cols = 2 ** (height + 1) - 1
+        tree_matrix = [["" for _ in range(cols)] for _ in range(rows)]
+        self.inorder(root, 0, (cols - 1) // 2, height, tree_matrix)
+        for row in tree_matrix:
+            for cell in row:
+                if cell == "":
+                    print(" ", end="")
+                else:
+                    print(cell, end="")
+            print()
+    
+    def inorder(self, root, row, col, height, ans):
+        if not root:
+            return
+
+        # Calculate offset for child positions
+        offset = 2 ** (height - row - 1)
+
+        # Traverse the left subtree
+        if len(root.branches) > 1:
+            self.inorder(root.branches[0], row + 1, col - offset, 
+                    height, ans)
+
+        # Place the current node's value in the matrix
+        ans[row][col] = str(root)
+
+        # Traverse the right subtree
+        if len(root.branches) > 1:
+            self.inorder(root.branches[1], row + 1, col + offset, 
+                    height, ans)
+    
+    def get_height(self, root):
+        if len(root.branches) < 1:
+            return 0
+        branch_heights = list(map(self.get_height, root.branches))
+        return max(branch_heights) + 1
+
+class TreeNode():
+    def __init__(self, operation, current_value, current_index, callibration):
+        self.operation = operation
+        self.value = current_value
+        self.branches = []
+        if current_index < len(callibration):
+            next_operation = callibration[current_index]
+            next_index = current_index + 1
+            self.branches = [
+                TreeNode('+', current_value + next_operation, next_index, callibration),
+                TreeNode('*', current_value * next_operation, next_index, callibration)
+            ]
+    
+    def __str__(self):
+        return f'{self.operation} {self.value}'.center(6)
--- 2024/day7/test.py
+++ 2024/day7/test.py
@@ -12,6 +12,19 @@ class TestBridgeCallibrator(unittest.TestCase):
 192: 17 8 14
 21037: 9 7 18 13
 292: 11 6 16 20'''
+
+    test_dictionary = {
+        190: [10, 19],
+        3267: [81, 40, 27],
+        83: [17, 5],
+        156: [15, 6],
+        7290: [6, 8, 6, 15],
+        161011: [16, 10, 13],
+        192: [17, 8, 14],
+        21037: [9, 7, 18, 13],
+        292: [11, 6, 16, 20],
+    }
+    
     def setUp(self):
         self.default_callibrator = callibrator.BridgeCallibrator(self.example_callibration_equations)
     
@@ -27,6 +40,22 @@ class TestBridgeCallibrator(unittest.TestCase):
             21037: [9, 7, 18, 13],
             292: [11, 6, 16, 20],
         }, self.default_callibrator.parse_callibration_string(self.example_callibration_equations))
+    
+    def test_should_find_correct_callibration_result_for_test_data(self):
+        self.assertEqual(3749, self.default_callibrator.get_total_callibration())
+    
+    def test_should_return_true_for_callibrations_with_valid_solutions(self):
+        self.assertTrue(self.default_callibrator.is_callibration_true(190, self.test_dictionary[190]))
+        self.assertTrue(self.default_callibrator.is_callibration_true(3267, self.test_dictionary[3267]))
+        self.assertTrue(self.default_callibrator.is_callibration_true(292, self.test_dictionary[292]))
+    
+    def test_should_return_false_for_callibrations_with_valid_solutions(self):
+        '''self.assertFalse(self.default_callibrator.is_callibration_true(83, self.test_dictionary[83]))
+        self.assertFalse(self.default_callibrator.is_callibration_true(156, self.test_dictionary[156]))
+        self.assertFalse(self.default_callibrator.is_callibration_true(7290, self.test_dictionary[7290]))
+        self.assertFalse(self.default_callibrator.is_callibration_true(161011, self.test_dictionary[161011]))
+        self.assertFalse(self.default_callibrator.is_callibration_true(192, self.test_dictionary[192]))
+        self.assertFalse(self.default_callibrator.is_callibration_true(21037, self.test_dictionary[21037]))'''
 
 if __name__ == '__main__':
     unittest.main()
