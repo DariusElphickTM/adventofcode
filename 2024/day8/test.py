da015cf963871df1f6a12da96cd8ed6b6b68098e
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-15 08:23:24 +0000

    Tests to help debug

--- 2024/day8/test.py
+++ 2024/day8/test.py
@@ -52,6 +52,35 @@ class TestHugeAntenna(unittest.TestCase):
         )
         self.default_test_antenna.with_harmonics = False
     
+    def test_returns_all_antinodes_for_frequency_with_harmonics(self):
+        test_huge_antenna = huge_antenna.HugeAntenna("""T....#....
+...T......
+.T....#...
+.........#
+..#.......
+..........
+...#......
+..........
+....#.....
+..........""")
+        test_huge_antenna.with_harmonics = True
+        result = test_huge_antenna.get_antinodes_for_frequency('T')
+        print(result)
+        self.assertListEqual([
+                {'x': 0, 'y': 0},
+                {'x': 3, 'y': 1},
+                {'x': 1, 'y': 1},
+                {'x': 5, 'y': 0}, #{'x': 5, 'y': 0}
+                {'x': 6, 'y': 2}, #{'x': 6, 'y': 2}
+                {'x': 9, 'y': 3}, #{'x': 9, 'y': 3}
+                {'x': 2, 'y': 4}, #{'x': 2, 'y': 4}
+                {'x': 3, 'y': 6}, #{'x': 3, 'y': 6},
+                {'x': 4, 'y': 8}  #{'x': 4, 'y': 8}
+            ], 
+            result
+        )
+        
+    
     def test_returns_all_antinodes_for_frequency(self):
         self.assertListEqual([
                 {'x': 4, 'y': 2},
