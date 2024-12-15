7cc2ede359b9531a92346e9cbd1ec82e5fca9e69
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-15 15:02:52 +0000

    Part 1 solution

---
+++ 2024/day9/solution.py
@@ -0,0 +1,17 @@
+from defragger import Defragger
+
+def main():
+    print("Here we go!")
+    input_string = read_file("input.txt")
+    defragger = Defragger(input_string)
+    print("Part 1 Result", defragger.get_file_system_checksum())
+
+def read_file(file_name):
+    """Reads a text file and returns all of it's contents."""
+    with open(file_name, encoding="utf-8") as file:
+        file_contents = file.read()
+        file.close()
+    return file_contents
+
+if __name__ == "__main__":
+    main()
