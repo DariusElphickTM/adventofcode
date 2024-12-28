7608aa7e914928dab34368eec4b53890895d65c1
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-29 06:52:52 +1300

    Nope, that doesn't work either

--- 2024/day11/solution.py
+++ 2024/day11/solution.py
@@ -1,12 +1,13 @@
-from stone_observer import StoneObserver
+from stone_observer import StoneObserver, MultiThreadedStoneObserver
 
 def main():
     print("Here we go!")
-    my_stone_observer = StoneObserver(read_file("input.txt"))
+    """my_stone_observer = StoneObserver(read_file("input.txt"))
     my_stone_observer.blink(25)
-    print("Part 1 result", my_stone_observer.get_stone_count())
-    my_stone_observer.blink(75)
-    print("Part 2 result", my_stone_observer.get_stone_count())
+    print("Part 1 result", my_stone_observer.get_stone_count())"""
+    
+    my_super_stone_observer = MultiThreadedStoneObserver(read_file("input.txt"))
+    print("Part 2 result", my_super_stone_observer.run_obsevers_in_parallel(75))
 
 def read_file(file_name):
     """Reads a text file and returns all of it's contents."""
