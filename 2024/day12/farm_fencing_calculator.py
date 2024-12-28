debbf89d3c6c0954a96634cabc2786073ba16a6c
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-29 10:03:11 +1300

    Set up failing tests for all of the example results

--- 2024/day12/farm_fencing_calculator.py
+++ 2024/day12/farm_fencing_calculator.py
@@ -2,6 +2,9 @@ class FarmFencingCalculator():
     def __init__(self, input_string):
         self.farm_map = self.parse_input(input_string)
     
+    def get_total_cost(self):
+        return 140
+    
     def parse_input(self, input_string):
         rows = input_string.splitlines()
         return list(map(list, rows))
--- 2024/day12/test.py
+++ 2024/day12/test.py
@@ -8,7 +8,13 @@ BBCD
 BBCC
 EEEC"""
 
-    larger_example_input = """RRRRIICCFF
+    larger_example_input = """OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"""
+
+    complex_example_input = """RRRRIICCFF
 RRRRIICCCF
 VVRRRCCFFF
 VVRCCCJFFF
@@ -20,7 +26,7 @@ MIIISIJEEE
 MMMISSJEEE"""
 
     def setUp(self):
-        self.default_test_calculator = FarmFencingCalculator(self.larger_example_input)
+        self.default_test_calculator = FarmFencingCalculator(self.complex_example_input)
     
     def test_parses_simple_example_correctly(self):
         test_calculator = FarmFencingCalculator(self.simple_example_input)
@@ -30,6 +36,18 @@ MMMISSJEEE"""
             ["B","B","C","C"],
             ["E","E","E","C"]
         ], test_calculator.farm_map)
+    
+    def test_returns_correct_cost_for_simple_example(self):
+        test_calculator = FarmFencingCalculator(self.simple_example_input)
+        self.assertEqual(test_calculator.get_total_cost(), 140)
+    
+    def test_returns_correct_cost_for_larger_example(self):
+        test_calculator = FarmFencingCalculator(self.larger_example_input)
+        self.assertEqual(test_calculator.get_total_cost(), 772)
+    
+    def test_returns_correct_cost_for_complex_example(self):
+        test_calculator = FarmFencingCalculator(self.complex_example_input)
+        self.assertEqual(test_calculator.get_total_cost(), 1930)
 
 if __name__ == "__main__":
     unittest.main()
