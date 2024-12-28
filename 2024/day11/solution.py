51ad27c38a2d2b7d477fb15c7cc87c8cbf58f730
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-29 05:44:45 +1300

    Part 1 solved

---
+++ 2024/day11/solution.py
@@ -0,0 +1,17 @@
+from stone_observer import StoneObserver
+
+def main():
+    print("Here we go!")
+    my_stone_observer = StoneObserver(read_file("input.txt"))
+    my_stone_observer.blink(25)
+    print("Part 1 result", my_stone_observer.get_stone_count())
+
+def read_file(file_name):
+    """Reads a text file and returns all of it's contents."""
+    with open(file_name, encoding="utf-8") as file:
+        file_contents = file.read()
+        file.close()
+    return file_contents
+
+if __name__ == "__main__":
+    main()
