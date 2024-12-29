2831d0c380cc1f8d79026be7d5758d70e03a0043
Darius Elphick <darius.elphick@trademe.co.nz>
2024-12-30 12:07:00 +1300

    Part 1 solved

---
+++ 2024/day12/solution.py
@@ -0,0 +1,17 @@
+from farm_fencing_calculator import FarmFencingCalculator
+
+def main():
+    print("Here we go!")
+    fencing_calculator = FarmFencingCalculator(read_file("input.txt"))
+    
+    print("Solution for part 1", fencing_calculator.get_total_cost())
+
+def read_file(file_name):
+    """Reads a text file and returns all of it's contents."""
+    with open(file_name, encoding="utf-8") as file:
+        file_contents = file.read()
+        file.close()
+    return file_contents
+
+if __name__ == "__main__":
+    main()
