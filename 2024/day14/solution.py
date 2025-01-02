c6d14dfbb94dbe3a5f4481a04d0ed16888ce6d2e
Darius Elphick <darius.elphick@trademe.co.nz>
2025-01-02 21:32:38 +1300

    Part 1 complete

---
+++ 2024/day14/solution.py
@@ -0,0 +1,16 @@
+from security_robot_tracker import SecurityRobotTracker
+
+def main():
+    robot_tracker = SecurityRobotTracker(read_file("input.txt"), 101, 103)
+    robot_tracker.track_robots(100)
+    print("Part 1 result", robot_tracker.get_safety_factor())
+
+def read_file(file_name):
+    """Reads a text file and returns all of it's contents."""
+    with open(file_name, encoding="utf-8") as file:
+        file_contents = file.read()
+        file.close()
+    return file_contents
+
+if __name__ == "__main__":
+    main()
