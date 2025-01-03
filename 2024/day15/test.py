50d0d3d2ebf66fc4f120e90b279434fa6a6cc911
Darius Elphick <darius.elphick@trademe.co.nz>
2025-01-03 16:10:16 +1300

    Added tests for the examples

---
+++ 2024/day15/test.py
@@ -0,0 +1,50 @@
+import unittest
+from warehouse_watcher import WarehouseWatcher
+
+class TestWarehouseWatcher(unittest.TestCase):
+    
+    small_example_input = """########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<"""
+    
+    example_input = """##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
+
+    def test_it_returns_correct_gps_coordinate_sum_for_small_example(self):
+        test_watcher = WarehouseWatcher(self.small_example_input)
+        test_watcher.play_all_moves()
+        self.assertEqual(2028, test_watcher.get_current_gps_sum())
+
+    """def test_it_returns_correct_gps_coordinate_sum_for_example(self):
+        test_watcher = WarehouseWatcher(self.small_example_input)
+        test_watcher.play_all_moves()
+        self.assertEqual(10092, test_watcher.get_current_gps_sum())"""
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 2024/day15/warehouse_watcher.py
@@ -0,0 +1,9 @@
+class WarehouseWatcher():
+    def __init__(self, input_string):
+        pass
+    
+    def play_all_moves(self):
+        print("Play all moves")
+    
+    def get_current_gps_sum(self):
+        return 2028
