231bffa6dde8399db77eae804fc09672a520e9aa
Darius Elphick <darius.elphick@trademe.co.nz>
2025-01-05 18:39:50 +1300

    Parsed input and created graph of the maze

--- 2024/day16/maze_runner.py
+++ 2024/day16/maze_runner.py
@@ -1,6 +1,58 @@
+import re
+
 class MazeRunner():
     def __init__(self, input_string):
-        pass
+        self.start_position = 0
+        self.end_position = 0
+        self.nodes = []
+        self.maze_adjacency_matrix = [[]]
+        
+        self.parse_input(input_string)
+    
+    def initialise_graph(self, size):
+        self.nodes = []
+        self.maze_adjacency_matrix = [[0] * size for _ in range(size)]
     
     def get_best_path_score(self):
         return 7036
+    
+    def add_edge(self, start, end, weight = 1):
+        self.maze_adjacency_matrix[start][end] = weight
+        self.maze_adjacency_matrix[end][start] = weight
+    
+    def parse_input(self, input_string):
+        input_grid = list(map(list, input_string.split('\n')))
+        column_height = len(input_grid)
+        row_length = len(input_grid[0])
+        
+        self.initialise_graph(len(re.sub('\n', '', input_string)))
+        
+        current_index = 0
+        for i, row in enumerate(input_grid):
+            for j, position in enumerate(row):
+                self.nodes.append(position)
+                
+                #Walls have no adjacencies
+                if not position == '#':
+                    if position == 'S':
+                        self.start_position = len(self.nodes) - 1
+                    elif position == 'E':
+                        self.end_position = len(self.nodes) - 1
+                    
+                    if i > 0 and input_grid[i - 1][j] == '.':
+                        #need to add adjacency above
+                        self.add_edge(current_index, current_index - row_length)
+                    
+                    if i < column_height - 1 and input_grid[i + 1][j] == '.':
+                        #need to add adjacency below
+                        self.add_edge(current_index, current_index + row_length)
+                    
+                    if j > 0 and input_grid[i][j - 1] == '.':
+                        #need to add adjacency to the left
+                        self.add_edge(current_index, current_index - 1)
+                    
+                    if j < row_length - 1 and input_grid[i][j + 1] == '.':
+                        #need to add adjecency to the right
+                        self.add_edge(current_index, current_index + 1)
+
+                current_index += 1
--- 2024/day16/test.py
+++ 2024/day16/test.py
@@ -18,6 +18,12 @@ class TestMazeRunner(unittest.TestCase):
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############"""
+    example_row_length = 15
+    expected_size = example_row_length*15
+    start_position = (example_row_length * 13) + 1
+    first_example_tile_position = (example_row_length * 2) + 1
+    second_example_tile_position = (example_row_length * 3) + 1
+    end_position = (example_row_length * 2) - 2
 
     second_example = """#################
 #...#...#...#..E#
@@ -36,6 +42,53 @@ class TestMazeRunner(unittest.TestCase):
 #.#.#.#########.#
 #S#.............#
 #################"""
+
+    def test_it_parses_input_and_generates_adjacency_matrix_for_first_example(self):
+        test_runner = MazeRunner(self.first_example)
+        
+        self.assertEqual(
+            self.start_position, test_runner.start_position
+        ) 
+        self.assertEqual(
+            self.end_position, test_runner.end_position
+        )
+        
+        self.assertEqual(self.expected_size, len(test_runner.nodes))
+        self.assertEqual('#', test_runner.nodes[0])
+        self.assertEqual('E', test_runner.nodes[self.end_position])
+        self.assertEqual('.', test_runner.nodes[self.first_example_tile_position])
+        self.assertEqual('.', test_runner.nodes[self.second_example_tile_position])
+        self.assertEqual('S', test_runner.nodes[self.start_position])
+        
+        self.assertEqual(self.expected_size, len(test_runner.maze_adjacency_matrix))
+        #Walls arent adjacent to anything
+        self.assertListEqual([0 for _ in range(self.expected_size)], test_runner.maze_adjacency_matrix[0])
+        
+        #The end is adjacent to two tiles
+        expected_end_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_end_adjacencies[self.end_position - 1] = 1
+        expected_end_adjacencies[self.end_position + self.example_row_length] = 1
+        self.assertListEqual(expected_end_adjacencies, test_runner.maze_adjacency_matrix[self.end_position])
+        
+        #First example tile is adjacent to two 
+        expected_first_tile_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_first_tile_adjacencies[self.first_example_tile_position - self.example_row_length] = 1
+        expected_first_tile_adjacencies[self.first_example_tile_position + self.example_row_length] = 1
+        self.assertListEqual(expected_first_tile_adjacencies, test_runner.maze_adjacency_matrix[self.first_example_tile_position])
+        
+        #Second example tile is adjacent to three 
+        expected_second_tile_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_second_tile_adjacencies[self.second_example_tile_position - self.example_row_length] = 1
+        expected_second_tile_adjacencies[self.second_example_tile_position + 1] = 1
+        expected_second_tile_adjacencies[self.second_example_tile_position + self.example_row_length] = 1
+        self.assertListEqual(expected_second_tile_adjacencies, test_runner.maze_adjacency_matrix[self.second_example_tile_position])
+        
+        #The start is adjacent to two tiles
+        expected_start_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_start_adjacencies[self.start_position + 1] = 1
+        expected_start_adjacencies[self.start_position - self.example_row_length] = 1
+        self.assertListEqual(expected_start_adjacencies, test_runner.maze_adjacency_matrix[self.start_position])
+        
 
     def test_it_returns_the_best_path_score_for_first_example(self):
         test_runner = MazeRunner(self.first_example)
