f41f00e556c4df1de0001c4cb91ecf70cc3f95f3
Darius Elphick <darius.elphick@trademe.co.nz>
2025-01-26 13:23:12 +1300

    Boilerplate for another maze runner using Dijkstra's algorithm

--- 2024/day16/maze_runner.py
+++ 2024/day16/maze_runner.py
@@ -108,3 +108,111 @@ class MazeRunner():
                         self.add_edge(current_index, current_index + 1, '>')
 
                 current_index += 1
+
+class DijkstraMazeRunner():
+    
+    def __init__(self, input_string):
+        self.start_position = 0
+        self.end_position = 0
+        self.nodes = []
+        self.row_length = 0
+        self.maze_adjacency_matrix = [[]]
+        
+        self.parse_input(input_string)
+    
+    def initialise_graph(self, size, row_length):
+        self.nodes = []
+        self.row_length = row_length
+        self.maze_adjacency_matrix = [[0] * size for _ in range(size)]
+    
+    def search_for_shortest_path_bfs(self):
+        visited = [False] * len(self.nodes)
+        results = []
+        visited[self.start_position] = True
+        queue = deque([{
+            'position': self.start_position,
+            'direction': '>',
+            'score': 0
+        }])
+        while queue:
+            current_step = queue.popleft()
+            current_position = current_step['position']
+            if current_position == self.end_position:
+                results.append(current_step['score'])
+                continue
+            
+            for i, adjacent_position_direction in enumerate(self.maze_adjacency_matrix[current_position]):
+                if adjacent_position_direction != 0 and not visited[i]:
+                    if i != self.end_position:
+                        visited[i] = True
+                    next_step = {
+                        'position': i,
+                        'direction': adjacent_position_direction,
+                        'score': current_step['score'] + 1
+                    }
+                    if adjacent_position_direction is not current_step['direction']:
+                        next_step['score'] = next_step['score'] + 1000
+                    queue.append(next_step)
+        results.sort()
+        return results[0]
+    
+    def get_map_with_visited(self, nodes, visited, row_length):
+        output = []
+        for i, node in enumerate(nodes):
+            if i % row_length == 0:
+                output.append('\n')
+            if visited[i] is not False:
+                output.append('*')
+            else:
+                output.append(node)
+        return "".join(output)
+    
+    def get_best_path_score(self):
+        return self.search_for_shortest_path_bfs()
+    
+    def add_edge(self, start, end, direction):
+        self.maze_adjacency_matrix[start][end] = direction
+        oppositeDirectionMap = {
+            '^': 'v',
+            'v': '^',
+            '<': '>',
+            '>': '<'
+        }
+        self.maze_adjacency_matrix[end][start] = oppositeDirectionMap[direction]
+    
+    def parse_input(self, input_string):
+        input_grid = list(map(list, input_string.split('\n')))
+        column_height = len(input_grid)
+        row_length = len(input_grid[0])
+        
+        self.initialise_graph(len(re.sub('\n', '', input_string)), row_length)
+        
+        current_index = 0
+        for i, row in enumerate(input_grid):
+            for j, position in enumerate(row):
+                self.nodes.append(position)
+                
+                #Walls have no adjacencies
+                if not position == '#':
+                    if position == 'S':
+                        self.start_position = len(self.nodes) - 1
+                    elif position == 'E':
+                        self.end_position = len(self.nodes) - 1
+                    
+                    if i > 0 and input_grid[i - 1][j] == '.':
+                        #need to add adjacency above
+                        self.add_edge(current_index, current_index - row_length, '^')
+                    
+                    if i < column_height - 1 and input_grid[i + 1][j] == '.':
+                        #need to add adjacency below
+                        self.add_edge(current_index, current_index + row_length, 'v')
+                    
+                    if j > 0 and input_grid[i][j - 1] == '.':
+                        #need to add adjacency to the left
+                        self.add_edge(current_index, current_index - 1, '<')
+                    
+                    if j < row_length - 1 and input_grid[i][j + 1] == '.':
+                        #need to add adjecency to the right
+                        self.add_edge(current_index, current_index + 1, '>')
+
+                current_index += 1
--- 2024/day16/test.py
+++ 2024/day16/test.py
@@ -1,5 +1,5 @@
 import unittest
-from maze_runner import MazeRunner
+from maze_runner import MazeRunner, DijkstraMazeRunner
 
 class TestMazeRunner(unittest.TestCase):
     
@@ -139,6 +139,146 @@ class TestMazeRunner(unittest.TestCase):
     
     def test_it_returns_the_best_path_score_for_reddit_test_case_2(self):
         test_runner = MazeRunner(self.reddit_test_case_2)
+        self.assertEqual(4013, test_runner.get_best_path_score())
+
+class TestDijkstraMazeRunner(unittest.TestCase):
+    
+    first_example = """###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############"""
+    example_row_length = 15
+    expected_size = example_row_length*15
+    start_position = (example_row_length * 13) + 1
+    first_example_tile_position = (example_row_length * 2) + 1
+    second_example_tile_position = (example_row_length * 3) + 1
+    end_position = (example_row_length * 2) - 2
+    
+    second_example = """#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################"""
+
+    reddit_test_case_1 = """###########################
+#######################..E#
+######################..#.#
+#####################..##.#
+####################..###.#
+###################..##...#
+##################..###.###
+#################..####...#
+################..#######.#
+###############..##.......#
+##############..###.#######
+#############..####.......#
+############..###########.#
+###########..##...........#
+##########..###.###########
+#########..####...........#
+########..###############.#
+#######..##...............#
+######..###.###############
+#####..####...............#
+####..###################.#
+###..##...................#
+##..###.###################
+#..####...................#
+#.#######################.#
+#S........................#
+###########################"""
+
+    reddit_test_case_2 = """##########
+#.......E#
+#.##.#####
+#..#.....#
+##.#####.#
+#S.......#
+##########"""
+
+    def test_it_parses_input_and_generates_adjacency_matrix_for_first_example(self):
+        test_runner = DijkstraMazeRunner(self.first_example)
+        
+        self.assertEqual(
+            self.start_position, test_runner.start_position
+        ) 
+        self.assertEqual(
+            self.end_position, test_runner.end_position
+        )
+        
+        self.assertEqual(self.expected_size, len(test_runner.nodes))
+        self.assertEqual('#', test_runner.nodes[0])
+        self.assertEqual('E', test_runner.nodes[self.end_position])
+        self.assertEqual('.', test_runner.nodes[self.first_example_tile_position])
+        self.assertEqual('.', test_runner.nodes[self.second_example_tile_position])
+        self.assertEqual('S', test_runner.nodes[self.start_position])
+        
+        self.assertEqual(self.expected_size, len(test_runner.maze_adjacency_matrix))
+        #Walls arent adjacent to anything
+        self.assertListEqual([0 for _ in range(self.expected_size)], test_runner.maze_adjacency_matrix[0])
+        
+        #The end is adjacent to two tiles
+        expected_end_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_end_adjacencies[self.end_position - 1] = '<'
+        expected_end_adjacencies[self.end_position + self.example_row_length] = 'v'
+        self.assertListEqual(expected_end_adjacencies, test_runner.maze_adjacency_matrix[self.end_position])
+        
+        #First example tile is adjacent to two 
+        expected_first_tile_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_first_tile_adjacencies[self.first_example_tile_position - self.example_row_length] = '^'
+        expected_first_tile_adjacencies[self.first_example_tile_position + self.example_row_length] = 'v'
+        self.assertListEqual(expected_first_tile_adjacencies, test_runner.maze_adjacency_matrix[self.first_example_tile_position])
+        
+        #Second example tile is adjacent to three 
+        expected_second_tile_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_second_tile_adjacencies[self.second_example_tile_position - self.example_row_length] = '^'
+        expected_second_tile_adjacencies[self.second_example_tile_position + 1] = '>'
+        expected_second_tile_adjacencies[self.second_example_tile_position + self.example_row_length] = 'v'
+        self.assertListEqual(expected_second_tile_adjacencies, test_runner.maze_adjacency_matrix[self.second_example_tile_position])
+        
+        #The start is adjacent to two tiles
+        expected_start_adjacencies = [0 for _ in range(self.expected_size)]
+        expected_start_adjacencies[self.start_position + 1] = '>'
+        expected_start_adjacencies[self.start_position - self.example_row_length] = '^'
+        self.assertListEqual(expected_start_adjacencies, test_runner.maze_adjacency_matrix[self.start_position])
+
+    def test_it_returns_the_best_path_score_for_first_example(self):
+        test_runner = DijkstraMazeRunner(self.first_example)
+        self.assertEqual(7036, test_runner.get_best_path_score())
+
+    def test_it_returns_the_best_path_score_for_second_example(self):
+        test_runner = DijkstraMazeRunner(self.second_example)
+        self.assertEqual(11048, test_runner.get_best_path_score())
+    
+    def test_it_returns_the_best_path_score_for_reddit_test_case_1(self):
+        test_runner = DijkstraMazeRunner(self.reddit_test_case_1)
+        self.assertEqual(21148, test_runner.get_best_path_score())
+    
+    def test_it_returns_the_best_path_score_for_reddit_test_case_2(self):
+        test_runner = DijkstraMazeRunner(self.reddit_test_case_2)
         self.assertEqual(4013, test_runner.get_best_path_score())
 
 if __name__ == "__main__":
